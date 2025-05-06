4489f9d4fe858c0431d3c4893cec51c96113d924
aureliacitra <citrapangukir@gmail.com>
2025-05-06 12:12:29 +0700

    add if

--- singly_linkedlist.cpp
+++ singly_linkedlist.cpp
@@ -28,4 +28,15 @@ class Linkedlist
         Node *nodeBaru = new Node;
         nodeBaru->noMhs = nim;
 
+        if (START == NULL || nim <= START->noMhs)
+        {
+            if((START != NULL) && (nim == START->noMhs))
+            {
+              cout << "\nDuplikasi noMhs tidak diijinkan\n";
+              return;
+            }
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
        
